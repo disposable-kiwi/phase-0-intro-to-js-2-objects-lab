d827cc55f29062d34ea56ac91fde885fa5356b51
Min <biffboffkev@gmail.com>
2022-06-18 18:01:31 -0400

    commit tree d798904582de988c7a93ef360835d4e02b65c1be

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+let employee = {
+    name:"XiaoLang",
+    streetAddress:"123 Main Street"
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...obj,
+        [key]:value
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key]=value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    let newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key];
+    return obj;
+}
